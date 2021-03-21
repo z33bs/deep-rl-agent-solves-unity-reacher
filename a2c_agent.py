7d4b9d2b5720054d9e6b107a0002f8f2b86e6a54
Zeebs <zeebsmail@gmail.com>
2021-03-21 05:31:54 +0200

    Add evaluation loop

--- a2c_agent.py
+++ a2c_agent.py
@@ -9,77 +9,18 @@ import torch.nn as nn
 import logger
 
 
-class BaseAgent:
+class A2CAgent:
     def __init__(self, config):
         self.config = config
         self.logger = logger.get_logger(tag=config.tag, log_level=config.log_level)
         self.task_ind = 0
 
-    def save(self, filename):
-        torch.save(self.network.state_dict(), '%s.model' % (filename))
-
-    def load(self, filename):
-        state_dict = torch.load('%s.model' % filename, map_location=lambda storage, loc: storage)
-        self.network.load_state_dict(state_dict)
-        with open('%s.stats' % (filename), 'rb') as f:
-            self.config.state_normalizer.load_state_dict(pickle.load(f))
-
-    def eval_episode(self):
-        env = self.config.eval_env
-        state = env.reset()
-        while True:
-            action = self.eval_step(state)
-            state, reward, done, info = env.step(action)
-            ret = info[0]['episodic_return']
-            if ret is not None:
-                break
-        return ret
-
-    def eval_episodes(self):
-        episodic_returns = []
-        for ep in range(self.config.eval_episodes):
-            total_rewards = self.eval_episode()
-            episodic_returns.append(np.sum(total_rewards))
-        self.logger.info('steps %d, episodic_return_test %.2f(%.2f)' % (
-            self.total_steps, np.mean(episodic_returns), np.std(episodic_returns) / np.sqrt(len(episodic_returns))
-        ))
-        self.logger.add_scalar('episodic_return_test', np.mean(episodic_returns), self.total_steps)
-        return {
-            'episodic_return_test': np.mean(episodic_returns),
-        }
-
-    # def record_online_return(self, info, offset=0):
-    #     if isinstance(info, dict):
-    #         ret = info['episodic_return']
-    #         if ret is not None:
-    #             self.logger.add_scalar('episodic_return_train', ret, self.total_steps + offset)
-    #             self.logger.info('steps %d, episodic_return_train %s' % (self.total_steps + offset, ret))
-    #     elif isinstance(info, tuple):
-    #         for i, info_ in enumerate(info):
-    #             self.record_online_return(info_, i)
-    #     else:
-    #         raise NotImplementedError
-
-class A2CAgent(BaseAgent):
-    def __init__(self, config):
-        BaseAgent.__init__(self, config)
         self.config = config
         self.env = Env('reacher.app')
-        self.network = GaussianActorCriticNet(
-            self.env.state_size, self.env.action_size)
+        self.network = GaussianActorCriticNet(self.env.state_size, self.env.action_size)
         self.optimizer = torch.optim.RMSprop(self.network.parameters(), lr=0.0007)
         self.total_steps = 0
         self.states, _, _ = self.env.reset(train_mode=False)
-
-    def tensor(self, x):
-        if isinstance(x, torch.Tensor):
-            return x
-        x = np.asarray(x, dtype=np.float32)
-        x = torch.from_numpy(x).to(Config.DEVICE)
-        return x
-
-    def to_np(self, t):
-        return t.cpu().detach().numpy()
 
     def step(self):
         config = self.config
@@ -140,3 +81,65 @@ class A2CAgent(BaseAgent):
          config.value_loss_weight * value_loss).backward()
         nn.utils.clip_grad_norm_(self.network.parameters(), config.gradient_clip)
         self.optimizer.step()
+
+    def save(self, filename):
+        torch.save(self.network.state_dict(), '%s.model' % (filename))
+
+    def load(self, filename):
+        state_dict = torch.load('%s.model' % filename, map_location=lambda storage, loc: storage)
+        self.network.load_state_dict(state_dict)
+        with open('%s.stats' % (filename), 'rb') as f:
+            self.config.state_normalizer.load_state_dict(pickle.load(f))
+
+    def eval_episode(self):
+        state , _, _ = self.env.reset(train_mode=False)
+        scores = np.zeros(self.env.num_agents)
+        while True:
+            self.network.eval()
+            prediction = self.network(state)  # 'action' 'log_pi_a' 'entropy' 'mean' 'v'
+            self.network.train()
+            reward, state, done = self.env.transition(self.to_np(prediction['action']))
+            # state, reward, done, info = env.step(action)
+            scores += reward
+            # ret = info[0]['episodic_return']
+            # if ret is not None:
+            #     break
+            if np.any(done):
+                break
+        return np.mean(scores)
+
+    def eval_episodes(self):
+        episodic_returns = []
+        for ep in range(self.config.eval_episodes):
+            total_rewards = self.eval_episode()
+            episodic_returns.append(np.sum(total_rewards))
+        self.logger.info('steps %d, episodic_return_test %.2f(%.2f)' % (
+            self.total_steps, np.mean(episodic_returns), np.std(episodic_returns) / np.sqrt(len(episodic_returns))
+        ))
+        # self.logger.add_scalar('episodic_return_test', np.mean(episodic_returns), self.total_steps)
+        return {
+            'episodic_return_test': np.mean(episodic_returns),
+        }
+
+    # def record_online_return(self, info, offset=0):
+    #     if isinstance(info, dict):
+    #         ret = info['episodic_return']
+    #         if ret is not None:
+    #             self.logger.add_scalar('episodic_return_train', ret, self.total_steps + offset)
+    #             self.logger.info('steps %d, episodic_return_train %s' % (self.total_steps + offset, ret))
+    #     elif isinstance(info, tuple):
+    #         for i, info_ in enumerate(info):
+    #             self.record_online_return(info_, i)
+    #     else:
+    #         raise NotImplementedError
+
+    def tensor(self, x):
+        if isinstance(x, torch.Tensor):
+            return x
+        x = np.asarray(x, dtype=np.float32)
+        x = torch.from_numpy(x).to(Config.DEVICE)
+        return x
+
+    def to_np(self, t):
+        return t.cpu().detach().numpy()
+
--- config.py
+++ config.py
@@ -12,11 +12,12 @@ class Config:
         self.entropy_weight = 0.01
         self.rollout_length = 5
         self.gradient_clip = 5
-        self.max_steps = int(1e3)  # int(2e7)
+        self.max_steps = int(2e7)
         self.log_level = 0
         self.tag = 'vanilla'
         self.value_loss_weight = 1.0
-        self.iteration_log_interval = 30
+        # self.iteration_log_interval = 30
         self.log_interval = int(1e3)
-        self.save_interval = 1e5
-        self.eval_interval = 0
+        self.save_interval = int(1e5)
+        self.eval_interval = int(1e3)
+        self.eval_episodes = 1
