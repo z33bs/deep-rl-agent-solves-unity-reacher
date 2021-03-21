659e628139b54b66155cc84e3b4729909e234c80
Zeebs <zeebsmail@gmail.com>
2021-03-21 11:12:16 +0200

    update config

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ import torch
 
 
 class Config:
-    DEVICE = torch.device('cpu')
+    DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     NOISY_LAYER_STD = 0.1
 
     def __init__(self):
         # note one episode lasts 100 000 steps
-        self.is_mock = True
+        self.is_mock = False
         self.discount = 0.99
         self.use_gae = True
         self.gae_tau = 1.0
@@ -17,6 +17,6 @@ class Config:
         self.log_level = 0
         self.tag = 'vanilla'
         self.value_loss_weight = 1.0
-        self.log_interval = 10000  # int(1e3)
+        self.log_interval = 20  # int(1e3)
         self.save_interval = None
-        self.max_episodes = 10
+        self.max_episodes = 1000
